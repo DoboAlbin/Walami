4eb40537c871f5fc59531e17e466e2fb39ba0459
Albin Dobó <albertoszimpaiersz@gmail.com>
2018-10-06 12:50:59 +0200

    Jejoy

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,17 +2,35 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int szam1 = Integer.parseInt(args[0]);
-		int szam2 = Integer.parseInt(args[1]);
 		
-		int osszeg = szam1 + szam2;
-		int kulonbseg = szam1 - szam2;
-		int szorzat = szam1 * szam2;
-		double hanyados = (double) szam1 / szam2;
+		int a = 10000000;
+				
+		if(a>=1 && a <=1000000) {
+			if(a>=1 && a<10) {
+				System.out.println("Egyjegyû");
+			}
+			else if (a>=10 && a<100) {
+				System.out.println("Kétjegyû");
+			}
+			else if (a>=100 && a<1000) {
+				System.out.println("Háromjegyû");
+			}
+			else if (a>=1000 && a<10000) {
+				System.out.println("Négyjegyû");
+			}
+			else if (a>=10000 && a<100000) {
+				System.out.println("Ötjegyû");
+			}
+			else if (a>=100000 && a<1000000) {
+				System.out.println("Hatjegyû");
+			}
+			else if (a==1000000) {
+				System.out.println("Hétjegyû");
+			}
+		}
+		else {
+			System.out.println("Nem megfelelõ szám");
+		}
 		
-		System.out.println("Összeg: " + osszeg);
-		System.out.println("Különbség: " + kulonbseg);
-		System.out.println("Szorzat: " + szorzat);
-		System.out.println("Hányados: " + hanyados);
 	}
 }
