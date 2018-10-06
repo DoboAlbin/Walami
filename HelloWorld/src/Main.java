8663ddb33ae1367d1f424aab2254ae1e6f4931ca
Albin Dobó <albertoszimpaiersz@gmail.com>
2018-10-06 11:22:37 +0200

    alapmuveletek

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,12 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-	int a = Integer.parseInt(args[0]);
-	int b = Integer.parseInt(args[1]);
-	int c = Integer.parseInt(args[2]);
-	System.out.println(a + " " + b + " " + c);
-	System.out.println(a+b+c);
-	System.out.println("Térfogat" + (a*b*c));
-	System.out.println(2*((a*b)+(b*c)+(a*c)));
+		int szam1 = Integer.parseInt(args[0]);
+		int szam2 = Integer.parseInt(args[1]);
+		
+		int osszeg = szam1 + szam2;
+		int kulonbseg = szam1 - szam2;
+		int szorzat = szam1 * szam2;
+		double hanyados = (double) szam1 / szam2;
+		
+		System.out.println("Összeg: " + osszeg);
+		System.out.println("Különbség: " + kulonbseg);
+		System.out.println("Szorzat: " + szorzat);
+		System.out.println("Hányados: " + hanyados);
 	}
 }
